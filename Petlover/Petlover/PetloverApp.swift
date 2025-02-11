aa4e2d1ca9cfbc86188c90d2d159fc56966eb86a
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2025-02-11 19:03:59 -0300

    Criado projeto e pastas inicias

---
+++ Petlover/Petlover/PetloverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetloverApp.swift
+//  Petlover
+//
+//  Created by Ian Pacini on 11/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PetloverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
