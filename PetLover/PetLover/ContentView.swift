11a4d02b5958ccee1d228b66ae2b0c6e505705ba
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2025-02-26 20:39:41 -0300

    Adicionei todas as cores customizadas do design system

---
+++ PetLover/PetLover/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetLover
+//
+//  Created by Ian Pacini on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetLover/PetLover/PetLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetLoverApp.swift
+//  PetLover
+//
+//  Created by Ian Pacini on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PetLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
