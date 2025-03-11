503b57b28b145efb774d769c9d772b57b1485e35
Izadora Montenegro <izadoraoamontenegro@gmail.com>
2025-03-10 21:23:13 -0300

    Criacao da indicator bar

---
+++ PetLover/PetLover/Views/Components/Buttons/IndicatorBar.swift
@@ -0,0 +1,32 @@
+//
+//  IndicatorBar.swift
+//  PetLover
+//
+//  Created by Izadora Montenegro on 10/03/25.
+//
+import SwiftUI
+
+struct IndicatorBar: View {
+    let numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<numberOfPages, id: \.self) { index in
+                Circle()
+                    .frame(width: 9, height: 9)
+                    .foregroundColor(currentPage == index ? Color.AppColors.primary50Orange : .white)
+                    .scaleEffect(currentPage == index ? 1.2 : 1.0)
+                    .animation(.easeInOut, value: currentPage)
+            }
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 6)
+        .background(
+            Color.AppColors.primary30Beige
+        )
+        .cornerRadius(20)
+        .padding()
+    }
+}
+
