b4faca96e5f1bcfac6de985a310b024efc5d762f
Izadora Montenegro <izadoraoamontenegro@gmail.com>
2025-03-11 19:50:18 -0300

    Add pet button

--- PetLover/PetLover/Views/Components/Buttons/AddPetButton.swift
+++ PetLover/PetLover/Views/Components/Buttons/AddPetButton.swift
@@ -7,4 +7,38 @@
 
 import SwiftUI
 
-//testando
+struct AddPetButton: View {
+    var body: some View {
+        VStack(spacing: 9) {
+            VStack {
+                ZStack {
+                    Circle()
+                        .stroke(Color.AppColors.primary40LightOrange, lineWidth: 6)
+                    Circle()
+                        .foregroundStyle(Color.AppColors.primary20NearWhite)
+                        .overlay(
+                            Image("Images/Buttons/AddButtonPetPaw")
+                                .resizable()
+                                .scaledToFit()
+                                .padding(.horizontal, 34)
+                        )
+                }
+            }
+            
+            
+            LargeButton(label: "Adicionar Pet") {
+                print("Peti adicionado")
+            }
+            .padding(.bottom, 310)
+        }
+        .padding(120)
+        .frame(maxWidth: .infinity)
+    }
+}
+
+//esses paddings 🤨
+
+
+#Preview {
+    AddPetButton()
+}
