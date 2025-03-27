66fec53bd9b4bb661a5ccd98fc501475d616408c
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2025-03-27 18:52:41 -0300

    Estrutura de pastas de arquivos

---
+++ PetLover/PetLover/Views/Home/Pet/Components/InlineCalendar/DayInlineCalendar.swift
@@ -0,0 +1,18 @@
+//
+//  DayInlineCalendar.swift
+//  PetLover
+//
+//  Created by Ian Pacini on 27/03/25.
+//
+
+import SwiftUI
+
+struct DayInlineCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DayInlineCalendar()
+}
---
+++ PetLover/PetLover/Views/Home/Pet/Components/InlineCalendar/InlineCalendar.swift
@@ -0,0 +1,18 @@
+//
+//  InlineCalendar.swift
+//  PetLover
+//
+//  Created by Ian Pacini on 27/03/25.
+//
+
+import SwiftUI
+
+struct InlineCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InlineCalendar()
+}
