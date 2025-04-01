6034c9db8b4a57157c8329f26dfbf1d3c6e90b8f
Izadora Montenegro <izadoraoamontenegro@gmail.com>
2025-03-31 21:15:44 -0300

    feat: cria o input para receber as datas de repeticoes do lembrete

---
+++ PetLover/PetLover/Views/Components/Inputs/InputButtons/ReminderRepeater.swift
@@ -0,0 +1,88 @@
+//
+//  ReminderRepeater.swift
+//  PetLover
+//
+//  Created by Izadora Montenegro on 31/03/25.
+//
+
+import SwiftUI
+
+struct ReminderRepeaterList: View {
+    @Binding var selectedDays: Set<WeekDays>
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Toggle(isOn: $isActive) {
+                Text("Repetir")
+                    .appFontDarkerGrotesque(darkness: .ExtraBold, size: 19)
+            }
+            .padding(.horizontal)
+            
+            HStack(spacing: 6) {
+                ForEach(WeekDays.allCases, id: \.self) { dia in
+                    ReminderRepeaterButton(selectedDays: $selectedDays, isActive: $isActive, dia: dia)
+                }
+            }
+        }
+        .padding()
+        .onChange(of: isActive) {
+            selectedDays.removeAll()
+        }
+    }
+}
+
+struct ReminderRepeaterButton: View {
+    @Binding var selectedDays: Set<WeekDays>
+    @Binding var isActive: Bool
+    
+    var dia: WeekDays
+    
+    var body: some View {
+        Button(action: {
+            if selectedDays.contains(dia) {
+                selectedDays.remove(dia)
+            } else {
+                selectedDays.insert(dia)
+            }
+        }) {
+            Text(dia.displayText)
+                .appFontDarkerGrotesque(darkness: .ExtraBold, size: 17)
+                .padding()
+                .background((selectedDays.contains(dia) && isActive) ? Color.AppColors.primary50Orange : Color.white)
+                .foregroundColor((selectedDays.contains(dia) && isActive) ? Color.white : Color.AppColors.secondary60BlueishGray)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 1)
+                        .foregroundColor(.AppColors.secondary60BlueishGray)
+                }
+        }
+        .frame(width: 44, height: 55)
+        .disabled(!isActive)
+    }
+}
+
+
+#Preview {
+    @Previewable @State var selectedDays: Set<WeekDays> = []
+    ReminderRepeaterList(selectedDays: $selectedDays)
+}
+
+
+// passar isso aqui pra dentro do modelo do lembrete depois
+enum WeekDays: String, Codable, CaseIterable {
+    case domingo, segunda, terca, quarta, quinta, sexta, sabado
+
+    var displayText: String {
+        switch self {
+        case .domingo: return "D"
+        case .segunda: return "S"
+        case .terca: return "T"
+        case .quarta: return "Q"
+        case .quinta: return "Q"
+        case .sexta: return "S"
+        case .sabado: return "S"
+        }
+    }
+}
